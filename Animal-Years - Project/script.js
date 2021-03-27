17fb59c981d4b3734ba56f13ca92c14c22d96583
GIZEM <80019972+gizemkk@users.noreply.github.com>
2021-03-26 20:35:00 -0700

    Add files via upload

---
+++ Animal-Years - Project/script.js
@@ -0,0 +1,16 @@
+// this is my age 
+var myAge=27;
+
+var earlyYears=2;
+
+ earlyYears= earlyYears*10.5
+console.log(earlyYears)
+
+var laterYears= myAge-2;
+console.log(laterYears)
+ laterYears*4;
+
+ var myAgeInDogYears= earlyYears + laterYears;
+
+ var myName='gizem'.toLowerCase();
+ alert(`my name is ${myName} i am ${myAge} years old whic is ${myAgeInDogYears} years old in dog years`)
